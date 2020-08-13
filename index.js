a2ebc2e7e58d22461a660b96230a91abe83ccf42
durbino <trey@buildingfame.com>
2020-08-13 11:13:42 -0700

    Completed Changes

--- index.js
+++ index.js
@@ -76,11 +76,13 @@ Your function should remove a flavor from the end of the array and console.log t
 
 For example removeLastFlavor(originalFlavors) would return ["Rainbow Sherbert", "Banana Nut Fudge",..."Vanilla"]*/
 
+
 function removeLastFlavor(array) {
   array.pop();
   console.log(array);
 }
 +removeLastFlavor(originalFlavors);
+
 
 /* Task 4: Write a function that returns a flavor at a given index in the array.
 
